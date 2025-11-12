275046344dce2cd3809eb943a6dde4ff0eb41c53
Kennro03 <kennro03@gmail.com>
2025-11-12 13:14:42 +0100

    Increase player speed and update movement logic
    Player speed was increased from 150.0 to 5000.0 and movement now scales with delta time. Added an unused _process function and mouse position retrieval in _physics_process.

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 const gravity : float = 9.81
 
-@export var speed : float = 150.0
+@export var speed : float = 5000.0
 @export var jump_power : float = 200.0
 
 var username : String
@@ -9,19 +9,23 @@ var username : String
 func _ready() -> void:
 	print("Player " + str(self) + " Ready.")
 
+func _process(_delta: float) -> void:
+	
+	pass
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	#velocity.y += delta * gravity * 20
 	velocity = Vector2.ZERO
 	if Input.is_action_pressed("right"):
-		velocity.x = speed
+		velocity.x = speed * _delta
 	if Input.is_action_pressed("left"):
-		velocity.x = -speed
+		velocity.x = -speed * _delta
 	if Input.is_action_pressed("down"):
-		velocity.y = speed
+		velocity.y = speed * _delta
 	if Input.is_action_pressed("up"):
-		velocity.y = -speed
+		velocity.y = -speed * _delta
 	
+	var mouse_position : Vector2 = get_global_mouse_position()
 	
 	
 	move_and_slide()
