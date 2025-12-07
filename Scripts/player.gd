591e46b9dbe1759263eab6b57ed42d838377b3be
Kennro03 <kennro03@gmail.com>
2025-12-07 18:48:53 +0100

    Switch player collision to circle and update movement
    Changed the player's CollisionShape2D from a rectangle to a circle in the scene file for more accurate collision. Refactored movement logic in player.gd to use a speed multiplier, adjusted default speed, and added sprite rotation to face the mouse position.

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,12 +1,14 @@
 extends CharacterBody2D
 const gravity : float = 9.81
+const speed_multiplier : float = 800.0
 
-@export var speed : float = 5000.0
+@export var speed : float = 10.0
 @export var jump_power : float = 200.0
 
 var username : String
 
 func _ready() -> void:
+	
 	print("Player " + str(self) + " Ready.")
 
 func _process(_delta: float) -> void:
@@ -17,15 +19,16 @@ func _physics_process(_delta: float) -> void:
 	#velocity.y += delta * gravity * 20
 	velocity = Vector2.ZERO
 	if Input.is_action_pressed("right"):
-		velocity.x = speed * _delta
+		velocity.x = speed * speed_multiplier * _delta
 	if Input.is_action_pressed("left"):
-		velocity.x = -speed * _delta
+		velocity.x = -speed * speed_multiplier * _delta
 	if Input.is_action_pressed("down"):
-		velocity.y = speed * _delta
+		velocity.y = speed * speed_multiplier * _delta
 	if Input.is_action_pressed("up"):
-		velocity.y = -speed * _delta
+		velocity.y = -speed * speed_multiplier * _delta
 	
 	var mouse_position : Vector2 = get_global_mouse_position()
-	
+	$Sprite2D.look_at(mouse_position)
+	$Sprite2D.global_rotation += deg_to_rad(90)
 	
 	move_and_slide()
